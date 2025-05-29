cdd40eec5ec37a7e7ac996ff6f5b8d3f31f89d9a
Samuel Thorson <sthor726@gmail.com>
2025-05-28 23:34:13 -0500

    Next head and image optimizations

--- testsites/next/next-test/src/pages/index.js
+++ testsites/next/next-test/src/pages/index.js
@@ -1,25 +1,31 @@
+import Image from "next/image";
 import Head from "next/head";
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
+        <title>{"title"}</title>
+        <meta name="description" content="description" />
         <meta name="robots" content="index, follow" />
-        <link rel="canonical" href={canonicalUrl} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={ogImage} />
+        <link rel="canonical" href="canonicalUrl" />
+        <meta property="og:title" content="title" />
+        <meta property="og:description" content="description" />
+        <meta property="og:image" content="ogImage" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content={canonicalUrl} />
+        <meta property="og:url" content="canonicalUrl" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={ogImage} />
+        <meta name="twitter:title" content="title" />
+        <meta name="twitter:description" content="description" />
+        <meta name="twitter:image" content="ogImage" />
       </Head>
       <h1>Home</h1>
       <a href="/about">About</a>
-      <img src="/vercel.svg" />
+      <Image
+        src="/vercel.svg"
+        width={500}
+        height={300}
+        alt="Image description"
+      />
     </div>
   );
 }
