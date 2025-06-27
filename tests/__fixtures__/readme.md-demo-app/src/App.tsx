e891bb24aa769a8e8058f0e9ee89edc048338c5c
Ryan Hermes <rryanhermes@gmail.com>
2025-06-27 00:30:44 -0500

    made it perfect for demo via donuts

--- tests/__fixtures__/readme.md-demo-app/src/App.tsx
+++ tests/__fixtures__/readme.md-demo-app/src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Welcome to My Bakery</h1>
+      <img src="/logo.png" />
+      <a href="https://examplebakery.com">click here</a>
+      <div>
+        <p>We serve the best donuts in town 🍩</p>
+      </div>
+    </div>
   );
 }
 
-export default App; 
+export default App;
